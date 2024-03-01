87491d3b3578a6cc22e95d1d3c4534864eba03b7
Mehmet Erim Gökhan <mehmeterimgokhan99@gmail.com>
2024-03-01 12:08:10 +0300

    text-left

--- components/Contactform.tsx
+++ components/Contactform.tsx
@@ -39,7 +39,7 @@ export default function ContactForm() {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 text-left">
         <FormField
           control={form.control}
           name="name"
